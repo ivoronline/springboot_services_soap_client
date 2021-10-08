c79c32eefbdff1830d3f631dbdbe1312d00f4e34
igoreta <ivoronline@gmail.com>
2021-10-08 15:36:15 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_services_soap_client/PersonClient.java
+++ src/main/java/com/ivoronline/springboot_services_soap_client/PersonClient.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Document;
-
 import java.net.URI;
 
 public class PersonClient {
@@ -18,35 +17,36 @@ public class PersonClient {
   // GET PERSON
   //============================================================================================
   public static String getPerson() throws Exception {
+
     //CREATE REQUEST OBJECT
-    GetPersonRequest    getPersonRequest  = new GetPersonRequest();
-                        getPersonRequest.setId(1);
+    GetPersonRequest    requestObject       = new GetPersonRequest();
+                        requestObject.setId(1);
 
     //MARSHAL REQUEST OBJECT
-    Document            requestDocument   = UtilXML.marshal(getPersonRequest, GetPersonRequest.class);
-    Document            requestSOAP       = UtilSOAP.createSOAPDocument(requestDocument);
-    String              requestString     = UtilXML.documentToString(requestSOAP);
+    Document            requestXMLDocument  = UtilXML.marshal(requestObject, GetPersonRequest.class);
+    Document            requestSOAPDocument = UtilSOAP.createSOAPDocument(requestXMLDocument);
+    String              requestSOAPString   = UtilXML.documentToString(requestSOAPDocument);
 
     //CONFIGURE REQUEST PARAMETERS
-    URI                 serverURI         = new URI(serverURL);
-    HttpHeaders         headers           = new HttpHeaders();
+    URI                 serverURI           = new URI(serverURL);
+    HttpHeaders         headers             = new HttpHeaders();
                         headers.set("Content-Type", "text/xml;charset=UTF-8");
-    HttpEntity<String>  entity            = new HttpEntity<>(requestString, headers);
+    HttpEntity<String>  entity              = new HttpEntity<>(requestSOAPString, headers);
 
     //SEND  REQUEST
-    RestTemplate        restTemplate      = new RestTemplate();
-    String              responseString    = restTemplate.postForObject(serverURI, entity, String.class);
+    RestTemplate        restTemplate        = new RestTemplate();
+    String              responseSOAPString  = restTemplate.postForObject(serverURI, entity, String.class);
 
     //UNMARSHAL RESPONSE XML INTO OBJECT
-    Document            responsetDocument = UtilSOAP.extractSOAPBody(responseString);
-    GetPersonResponse   getPersonResponse = (GetPersonResponse) UtilXML.unmarshal(responsetDocument, GetPersonResponse.class);
+    Document            responseXMLDocument = UtilSOAP.extractSOAPBody(responseSOAPString);
+    GetPersonResponse   responseObject      = (GetPersonResponse) UtilXML.unmarshal(responseXMLDocument, GetPersonResponse.class);
 
     //GET PERSON NAME
-    String              name              = getPersonResponse.getName();
+    String              name                = responseObject.getName();
 
     //DISPLAY REQUEST & RESPONSE
-    System.out.println(requestString);
-    System.out.println(responseString);
+    System.out.println(requestSOAPString );
+    System.out.println(responseSOAPString);
 
     //RETURN RESPONSE OBJECT
     return "Hello " + name;
